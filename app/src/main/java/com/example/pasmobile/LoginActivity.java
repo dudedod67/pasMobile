1219f46d7cfbaaac7a9e1cafaf16c00e15de2543
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2023-05-27 08:55:51 +0700

    first commit

---
+++ app/src/main/java/com/example/pasmobile/LoginActivity.java
@@ -0,0 +1,89 @@
+package com.example.pasmobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextUsername, editTextPassword;
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextUsername = findViewById(R.id.txtUsername);
+        editTextPassword = findViewById(R.id.txtPassword);
+        buttonLogin = findViewById(R.id.loginbtn);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                //hit API Login
+                AndroidNetworking.post("https://mediadwi.com/api/latihan/login?username=admin&password=admin")
+                        .addBodyParameter("username", username)
+                        .addBodyParameter("password", password)
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                // Handle the response
+                                // ...
+                                Log.d("Login Sukses!!!!!!!!!!", "onResponse: "+response.toString());
+                                try {
+                                    boolean status = response.getBoolean("status");
+                                    String message = response.getString("message");
+                                    if (status){
+                                        // Login berhasil
+                                        Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                        // Lanjutkan ke aktivitas selanjutnya
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }else{
+                                        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                buttonLogin.setEnabled(true);
+                            }
+
+                            @Override
+                            public void onError(ANError error) {
+                                Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+//                // Lakukan verifikasi login di sini
+//                if (username.equals("admin") && password.equals("admin")) a{
+//
+//                } else {
+//                    // Login gagal
+//                    Toast.makeText(Login.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+//                }
+            }
+   });
+}
+}
